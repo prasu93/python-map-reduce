413252f5fbd1a7fcd54ce1b662165fbcd5651e8c
Prasannakumar Potnuru <S537361@nwmissouri.edu>
2020-10-06 19:53:58 -0500

    Aggregated using department;changed code

--- 11mapper.py
+++ 11mapper.py
@@ -4,7 +4,7 @@
 
 # open returns a file object
 with open("part.txt", "r") as input:
-  with open("output11mapped.txt", "w") as output:
+  with open("outputCase1_1.txt", "w") as output:
 
     # iterate through each line in the file object
     for line in input:
@@ -13,8 +13,8 @@ with open("part.txt", "r") as input:
         date, time,store, department, amount, paymentType = datalist
 
         # output intermediate key-value pairs
-        output.write(store + "\t" + amount + "\n")
+        output.write(department + "\t" + amount + "\n")
 
         # display to console (not required - just for the user)
-        print(store + "\t" + amount + "\n")
+        print(department + "\t" + amount + "\n")
 
